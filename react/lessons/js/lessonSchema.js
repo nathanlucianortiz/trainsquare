be1c4318c9275d7299c52587697ca80229bb3e4f
Nathan Ortiz <71405469+nathanlucianortiz@users.noreply.github.com>
2022-05-11 12:25:28 -0700

    Add files via upload

--- react/lessons/js/lessonSchema.js
+++ react/lessons/js/lessonSchema.js
@@ -1,12 +1,12 @@
 import * as Yup from 'yup';
 
-const basicSchema = Yup.object().shape({
+const lessonSchema = Yup.object().shape({
     title: Yup.string().min(2).max(50).required('Please add a lesson title.'),
-    description: Yup.string().min(2).max(500).required('Please add the lesson description.'),
-    durationTypeId: Yup.number().min(1).max(50).required('Please add the duration of the lesson.'),
+    description: Yup.string().min(2).max(500).required('Please add a lesson description.'),
+    durationTypeId: Yup.number().min(1).max(50).required('Please select a lesson duration.'),
     imageUrl: Yup.string().min(0).max(150).nullable(true),
     fileUrl: Yup.string().min(2).max(125).required('Please provide a file.'),
-    sortOrder: Yup.number().min(1).max(3).required('Please select a skill level.'),
+    sortOrder: Yup.number().min(1).max(3).required('Please add the order of appearance.'),
 });
 
-export default basicSchema;
+export default lessonSchema;
